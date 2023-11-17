042a2f57ece42244664adfcd0b20d214af7874b4
Yehudah Christian <121639934+PapiCLY@users.noreply.github.com>
2023-11-17 00:06:47 -0500

    updated install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,34 +1,39 @@
 const butInstall = document.getElementById('buttonInstall');
-let deferredPrompt;
 
 // Logic for installing the PWA
 // Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  event.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = event;
-  // Update UI notify the user they can add to home screen
-  butInstall.style.display = 'block';
+    // Prevent Chrome <= 67 from automatically showing the prompt
+    event.preventDefault();
+    // Stash the event so it can be triggered later.
+    window.deferredPrompt = event;
+    // Show the install button
+    butInstall.style.display = 'block';
 });
 
 // Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-  // hide our user interface that shows our A2HS button
-  butInstall.style.display = 'none';
-  // Show the prompt
-  deferredPrompt.prompt();
-  // Wait for the user to respond to the prompt
-  const choiceResult = await deferredPrompt.userChoice;
-  if (choiceResult.outcome === 'accepted') {
-    console.log('User accepted the A2HS prompt');
-  } else {
-    console.log('User dismissed the A2HS prompt');
-  }
-  deferredPrompt = null;
+    // Check if there's no deferredPrompt to handle
+    if (!window.deferredPrompt) {
+        return;
+    }
+    // Show the prompt
+    window.deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    const choiceResult = await window.deferredPrompt.userChoice;
+    if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+    } else {
+        console.log('User dismissed the A2HS prompt');
+    }
+    // We no longer need the prompt.  Clear it up.
+    window.deferredPrompt = null;
+    butInstall.style.display = 'none';
 });
 
 // Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-  console.log('PWA was installed');
+    console.log('PWA was installed');
+    window.deferredPrompt = null;
+    butInstall.style.display = 'none';
 });
