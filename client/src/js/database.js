39f59f42293096145cb3621ca7e7b713f450c25f
Yehudah Christian <121639934+PapiCLY@users.noreply.github.com>
2023-11-16 23:40:43 -0500

    updated database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -3,19 +3,28 @@ import { openDB } from 'idb';
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
-        return;
+      if (!db.objectStoreNames.contains('jate')) {
+        db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+        console.log('jate database created');
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+const dbPromise = initdb();
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const putDb = async (content) => {
+  const db = await dbPromise;
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  await store.add(content);
+  return tx.done;
+};
+
+export const getDb = async () => {
+  const db = await dbPromise;
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  return store.getAll();
+};
 
 initdb();
